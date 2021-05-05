250fec36236f4fb56234e4d145af0d1e330ec06c
Daniel <nefesh255@gmail.com>
2021-05-05 16:37:07 +0300

    ESP_now setup need update

--- ESP_32_WEIGHT_SCALE.ino
+++ ESP_32_WEIGHT_SCALE.ino
@@ -12,7 +12,48 @@ HX711_ADC LoadCell(HX711_dout, HX711_sck);
 
 // const int calVal_eepromAdress = 0;
 unsigned long t = 0;
+String test = "testnSTRIng";
+float weight;
 
+/*____________________________________ESP_NOW_________________*/
+// REPLACE WITH THE MAC Address of your receiver 
+uint8_t broadcastAddress[] = {0xFC, 0xF5, 0xC4, 0x0D, 0xD6, 0x48};
+// Define variables to store incoming measage
+String incomingMsg;
+
+typedef struct struct_message {
+    float weight;
+    String s;
+} struct_message;
+
+struct_message WeightReadings;
+
+typedef struct struct_message2 {
+    String Msg;
+} struct_message2;
+
+struct_message2 incomingMessages;
+
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+// Callback when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&WeightReadings, incomingData, sizeof(WeightReadings));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  incomingMsg = incomingMessages.Msg;
+  Serial.println(incomingMsg);
+}
+void printMessage(){
+  // Display Readings in Serial Monitor
+  Serial.println();
+  Serial.print("ESP32-WROOM GOT MESSAGE:   ");
+  Serial.print(incomingMsg);
+}
+/*____________________________________ESP_NOW_end_________________*/
 void setup() {
   Serial.begin(115200); delay(10);
   Serial.println();
@@ -36,8 +77,35 @@ void setup() {
     LoadCell.setCalFactor(calibrationValue); // set calibration value (float)
     Serial.println("Startup is complete");
   }
-/*___________________________________________________*/
+/*____________________________________________________________*/
+/*___________________________ESP_NOW__________________________*/
+// Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  WiFi.begin();
+  //WiFi.disconnect();
 
+  // Init ESP-NOW
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Set ESP-NOW Role
+ // esp_now_set_self_role(ESP_NOW_ROLE_COMBO); 
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+ //TODO
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  
+  // Register for a callback function that will be called when data is received
+  esp_now_register_recv_cb(OnDataRecv);
+  /*_______________________________ESP_NOW_end___________________________*/
 }
 
 void loop() {
